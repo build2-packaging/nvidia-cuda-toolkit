79e98158628f08165d06731e10be2166c0cb0bac
helmesjo <helmesjo@gmail.com>
2025-03-08 00:07:44 +0100

    libcccl doesn't have any LICENSE file, so don't expect one.

--- libcccl/tests/basics/driver.c
+++ libcccl/tests/basics/driver.c
@@ -1,6 +1,10 @@
 #include <cuda/version>
 
 #undef NDEBUG
-#include <cassert>
 
-int main() {}
+#include <cassert>
+#include <stdio.h>
+
+int main() {
+  printf("libcccl - _CCCL_STD_VER: %d\n", _CCCL_STD_VER);
+}

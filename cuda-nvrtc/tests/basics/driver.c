207c02c8e6c29e2dca4b8e8a5c59c38a5466c5c4
helmesjo <helmesjo@gmail.com>
2025-09-26 18:19:23 +0200

    Rename 'libnvrtc' to canonical (upstream) name 'cuda-nvrtc'.

--- cuda-nvrtc/tests/basics/driver.c
+++ cuda-nvrtc/tests/basics/driver.c
@@ -8,6 +8,6 @@
 int main() {
   int major=0, minor=0;
   assert(nvrtcVersion(&major, &minor) == NVRTC_SUCCESS);
-  printf("libnvrtc - nvrtcVersion(): v%d.%d\n", major, minor);
+  printf("cuda-nvrtc - nvrtcVersion(): v%d.%d\n", major, minor);
   return 0;
 }

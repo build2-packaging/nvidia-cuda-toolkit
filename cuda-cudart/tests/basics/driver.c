0088879f8b480b0f9aa408cb64032723388a678a
helmesjo <helmesjo@gmail.com>
2025-10-30 02:14:07 +0100

    cuda-cudart: Re-add device iteration in test (if any is found).

--- cuda-cudart/tests/basics/driver.c
+++ cuda-cudart/tests/basics/driver.c
@@ -6,7 +6,7 @@
 
 #include <assert.h>
 #include <stdio.h>
-// #include <string.h>
+#include <string.h>
 
 int main() {
   int version = 0;
@@ -28,17 +28,17 @@ int main() {
   printf("cuda-cudart - cudaGetDeviceCount(): %d\n", count);
 
   /* Only available on system with Nvidia GPUs */
-  // int len = 1024;
-  // char* name = (char*)malloc(len * sizeof(char));
-  // for(int i = 0; i < count; ++i)
-  // {
-  //   CUdevice dev;
-  //   assert(cuDeviceGet(&dev, i) == CUDA_SUCCESS);
-  //   memset(name, 0, len);
-  //   assert(cuDeviceGetName(name, len, dev) == CUDA_SUCCESS);
-  //   printf("cuda-cudart - cuDeviceGetName(): %s (device: %d)\n", name, dev);
-  // }
-  // free(name);
+  int len = 1024;
+  char* name = (char*)malloc(len * sizeof(char));
+  for(int i = 0; i < count; ++i)
+  {
+    CUdevice dev;
+    assert(cuDeviceGet(&dev, i) == CUDA_SUCCESS);
+    memset(name, 0, len);
+    assert(cuDeviceGetName(name, len, dev) == CUDA_SUCCESS);
+    printf("cuda-cudart - cuDeviceGetName(): %s (device: %d)\n", name, dev);
+  }
+  free(name);
 
   // expects the driver version as input
   if(version > 0)
